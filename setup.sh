c4f16b42695aca7fa52bb403b295636edb9ac95e
benrau87 <benrau87@users.noreply.github.com>
2017-05-19 16:30:23 -0500

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+me=whoami
+
+export DEBIAN_FRONTEND=noninteractive
+
+apt-get update && apt-get install -y ubuntu-desktop xfce4 xrdp xfce4 xfce4-goodies
+echo xfce4-session > ~/.xsession
+ 
+ cp /home/$me/.xsession /etc/skel 
+ 
+ sed -i 's/port=-1/port=ask-1/g' /etc/xrdp/xrdp.ini
+ 
+ service xrdp restart
