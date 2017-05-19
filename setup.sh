22bd35d86b70a3ee6e99a73ac9496b6e14de3bd3
benrau87 <benrau87@users.noreply.github.com>
2017-05-19 16:38:46 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get update && apt-get install -y ubuntu-desktop xfce4 xrdp xfce4 xfce4-goodies
 echo xfce4-session > ~/.xsession
  
- cp /home/$me/.xsession /etc/skel 
+ cp /home/ubuntu/.xsession /etc/skel 
  
  sed -i 's/port=-1/port=ask-1/g' /etc/xrdp/xrdp.ini
  
